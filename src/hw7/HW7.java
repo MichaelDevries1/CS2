e732ff11cdc5b5fdf09804e0a6ae921da1943a1b
michaeldevries1 <Tu35%^dMpXhQWrA2!^F^9fxzP59CkHQ1q87@hPgN>
2017-11-13 19:26:02 -0700

    This should be the final version
    Updated how the hash is calculated and removed a significant portion of
    the unused table size. Also worked out how to avoid running over the
    size of the table with the hash.

--- src/hw7/HW7.java
+++ src/hw7/HW7.java
@@ -18,10 +18,10 @@ public class HW7 {
 		String location = mt.fileLocation(scn);
 		String fullDocument = null;
 		List<String> unique = new ArrayList<>();
+		final String TESTOUTPUT = "HW7out.txt";
 		
 		mt.p("What prime number would you like to hash with? ", 0);
 		int prime = scn.nextInt();
-		
 		
 		prime = mt.closestPrime(prime); 
 				
@@ -33,32 +33,34 @@ public class HW7 {
 		} // end Try/Catch
 		
 		try {
-			PrintWriter out = new PrintWriter (new FileWriter("HW7out.txt"));
+			PrintWriter out = new PrintWriter (new FileWriter(TESTOUTPUT));
 		
 		
 		unique = ui.findUnique(fullDocument);
 		
-		Hashtable<Integer, String> internal = new Hashtable<Integer, String>();
-		MyHashTable h = new MyHashTable(unique.size());
+		Hashtable<Integer, String> internal = new Hashtable<Integer, String>(unique.size());
+		MyHashTable h = new MyHashTable((int)(unique.size()/5.5));
+		
+		h.setPrime(prime);
 		
 		Iterator<String> itr = unique.iterator();
 		
 		while (itr.hasNext()) {
 			String word = itr.next();
-			h.put(word, prime);
+			h.put(word);
 			internal.put(prime, word);
 		} // end while
 		
 		out.println("Personal HashTable: ");
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < h.size(); i++) {
 			h.printList(i, out);
 		} // end for
 		
-		out.println("\n\nInternal HashTable: ");
-		for (int i = 0; i < 5; i++) {
-			out.print("\tKey " + i + ": ");
-			out.println("" + internal.get(i));
-		}
+//		out.println("\n\nInternal HashTable: ");
+//		for (int i = 0; i < 5; i++) {
+//			out.print("\tKey " + i + ": ");
+//			out.println("" + internal.get(i));
+//		}
 		out.close();
 		
 		} catch (IOException e) {
--- src/toolbox/MyHashTable.java
+++ src/toolbox/MyHashTable.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 public class MyHashTable {
 	private MyLL[] hashArray;
 	private int size;
+	private double alpha = 5.51;
+	private int prime = 0;
 	
 	public MyHashTable (int n) {
-		size = n;
+		size = (int)(n/alpha);
 		hashArray = new MyLL[size];
 		for (int i = 0; i < size; i++) {
 			hashArray[i] = new MyLL();
@@ -16,19 +18,36 @@ public class MyHashTable {
 	
 	//====================================================
 	
-	private int computeHash (String s, int prime) {
+	public void setPrime (int sp) {
+		prime = sp;
+	} // end setPrime
+	
+	//====================================================
+	
+	public int size () {
+		return size;
+	}
+	//====================================================
+	
+	private int computeHash (String s) {
 		int hash = 0;
 		
 		for (int i = 0; i < s.length(); i++) {
 			hash += s.charAt(i);
 		} // end for
-		return hash % (size);
+		hash = hash % prime;
+		
+		while (hash > size) {
+			hash -= size;
+		} // end while
+		
+		return hash;
 	} // end computeHash
 	
 	//====================================================
 	
-	public boolean containsString (String target, int prime) {
-		int hash = computeHash(target, prime);
+	public boolean containsString (String target) {
+		int hash = computeHash(target);
 		MyLL list = hashArray[hash];
 		if (!list.contains(target)) {
 			return true;
@@ -38,8 +57,8 @@ public class MyHashTable {
 	
 	//====================================================
 	
-	public void put (String s, int prime) {
-		int hash = computeHash(s, prime);
+	public void put (String s) {
+		int hash = computeHash(s);
 		 MyLL list = hashArray[hash];
 		 if (!list.contains(s)) {
 			 hashArray[hash].addFirst(s);
